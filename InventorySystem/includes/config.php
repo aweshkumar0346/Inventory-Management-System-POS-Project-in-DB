e4c41ada0e8d78b467c3ebfe0f2bd519d0afed39
Awesh Kumar <162352345+aweshkumar0346@users.noreply.github.com>
2024-04-20 18:49:15 +0500

    Add files via upload

---
+++ InventorySystem/includes/config.php
@@ -0,0 +1,17 @@
+<?php
+/*
+|--------------------------------------------------------------------------
+| OWSA-INV V2
+|--------------------------------------------------------------------------
+| Project Name: OSWA-INV
+| Version: v2
+|
+|
+|
+*/
+  define( 'DB_HOST', 'localhost' );          // Set database host
+  define( 'DB_USER', 'root' );             // Set database user
+  define( 'DB_PASS', '' );             // Set database password
+  define( 'DB_NAME', 'inventory_system' );        // Set database name
+
+?>
